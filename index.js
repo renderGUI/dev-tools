77e816925f903aea4099ecabd31509459dc34ad7
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-05-02 07:08:39 -0400

    Create application menu

--- index.js
+++ index.js
@@ -2,3 +2,36 @@
 
 import chalk from "chalk";
 import inquirer from "inquirer";
+
+// DEVTOOLS MENU
+async function devToolsMenu() {
+  console.clear();
+  const answers = await inquirer.prompt({
+    name: "chosenTool",
+    type: "list",
+    message: "WELCOME TO DEVTOOLS!  SELECT A TOOL TO USE:",
+    choices: [
+      "Random Color Generator",
+      "Coin Flipper",
+      "Decimal-to-Binary Converter",
+      "Git Commands Reference Sheet",
+    ],
+  });
+
+  let chosenOption = answers.chosenTool;
+  switch (chosenOption) {
+    case "Random Color Generator":
+      runRandomColorGenerator();
+      break;
+    case "Coin Flipper":
+      runCoinFlipper();
+      break;
+    case "Decimal-to-Binary Converter":
+      runDecimalToBinaryConverter();
+      break;
+    case "Git Commands Reference Sheet":
+      runGitCommands();
+      break;
+  }
+}
+await devToolsMenu();
